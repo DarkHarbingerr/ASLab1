5f6350763017c87bd3715e81a1d4f3c1a7f36dc5
DarkHarbingerr <kroman052@gmail.com>
2020-03-09 22:57:12 +0300

    Initial commit

---
+++ app/src/main/java/com/komurdzhiev/myapplication/DisplayTextActivity.java
@@ -0,0 +1,48 @@
+package com.komurdzhiev.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+public class DisplayTextActivity extends AppCompatActivity {
+
+    public static final String VARIABLE_KEY = "DisplayTextActivity.VARIABLE_KEY";
+    public static final String INPUT_TEXT = "DisplayTextActivity.INPUT_TEXT";
+    public static final String TEXTVIEW_KEY = "TextView_Text";
+
+    private String text = "";
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+        Bundle arg = getIntent().getExtras();
+        if (arg != null) {
+            textView = findViewById(R.id.textview_out);
+            textView.setMovementMethod(new ScrollingMovementMethod());
+            text = arg.getString(INPUT_TEXT);
+            textView.setText(text);
+        }
+    }
+
+    //Save Data
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString(VARIABLE_KEY, text);
+        textView = findViewById(R.id.textview_out);
+        outState.putString(TEXTVIEW_KEY, textView.getText().toString());
+        super.onSaveInstanceState(outState);
+    }
+
+    //Restore Data
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        text = savedInstanceState.getString(VARIABLE_KEY);
+        String textViewText = savedInstanceState.getString(TEXTVIEW_KEY);
+        textView = findViewById(R.id.textview_out);
+        textView.setText(textViewText);
+    }
+}
---
+++ app/src/main/java/com/komurdzhiev/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.komurdzhiev.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button nextPageBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nextPageBtn = findViewById(R.id.btn_next);
+        editText = findViewById(R.id.et_input_field);
+
+        nextPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, DisplayTextActivity.class);
+                intent.putExtra(DisplayTextActivity.INPUT_TEXT, text);
+                startActivity(intent);
+            }
+        });
+    }
+}
